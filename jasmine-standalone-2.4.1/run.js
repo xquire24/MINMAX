660576de030814051f9ce470c632073a1c9cd804
charles <xquireworld@gmail.com>
2017-01-07 12:10:22 +0000

    checking for equality

--- jasmine-standalone-2.4.1/run.js
+++ jasmine-standalone-2.4.1/run.js
@@ -4,3 +4,13 @@ var findMinMax = function(list){
         var Max = Math.max.apply(null, list);
         var MinNMax = [Min,Max]
         var  is_equal = []
+
+//checking whether the element is equal to itself
+        for(i= 0; i < num.length; i++){
+            if(num[i] == num[i+1]){
+                 is_equal.push(num[i])
+                return is_equal
+            }
+            else return MinNMax
+        }
+    }
